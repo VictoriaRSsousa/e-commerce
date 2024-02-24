7a77745a85dcb56e557f978dc7324044c4e9c267
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-02-23 23:38:50 -0300

    attTelas

--- e-commerce/src/pages/Produto/index.jsx
+++ e-commerce/src/pages/Produto/index.jsx
@@ -1,29 +1,31 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import { BiSolidUpArrow,BiSolidDownArrow  } from "react-icons/bi";
+
 
 export default function Produto() {
   return (
     <>
       <Header />
 
-      <main className=" flex justify-center items-center shadow-md">
-        <body className="w-4/6 h-2/5 flex items-center m-40  bg-slate-100 p-7">
+      <main className=" flex justify-center items-center  ">
+        <body className="	 flex flex-col md:flex-row  md:items-center mt-14 md:m-40 shadow-lg bg-slate-100 p-6 md:p-7 gap-2 md:gap-16 font-Inter">
           <section className="">
             <img
               src="https://m.media-amazon.com/images/I/71JtGDJmz7L._AC_SY741_.jpg"
-              className="w-[309px]	 h-[342px]"
+              className=" w-[223px] md:w-[309px]	 h-[172px] md:h-[342px]"
               alt=""
             />
-            <span>
-              <p>Quantidades Disponíveis</p>
-              <p>100 itens Disponíveis</p>
+            <span className="text-2xl	hidden md:flex flex-col gap-6	mt-10">
+              <p className="text-azul-escuro font-semibold ">Quantidades Disponíveis</p>
+              <p className="font-medium	text-stone-500 ">100 itens Disponíveis</p>
             </span>
           </section>
           <section className=" ">
-            <div>
-              <h3>Smart Watt</h3>
+            <article className="font-medium	text-sm	md:text-2xl text-stone-500	flex flex-col gap-2">
+              <h3 className="font-semibold text-azul-escuro text-4xl	">Smart Watt</h3>
               <p>Relógio</p>
-              <p className=" 	">
+              <p className="w-[224px] md:w-[514px] ">
                 O smartwatch é o seu companheiro ideal para uma vida mais
                 saudável, conectada e cheia de estilo. Com ele, você recebe
                 notificações do seu smartphone, monitora sua saúde (batimentos
@@ -32,14 +34,21 @@ export default function Produto() {
                 desfruta de um design elegante e moderno com diversas pulseiras
                 intercambiáveis.{" "}
               </p>
-            </div>
-            <div>
-              <span>
-                <p>quantidade</p>
-                <p>01</p>
+            </article>
+            <article className="flex flex-col md:flex-row items-center gap-7 my-10">
+              <span className="flex flex-col items-center">
+                <p className="font-semibold text-2xl text-black">Quantidade:</p>
+                <div className="flex bg-white border border-black rounded-md h-10 w-24	justify-center items-center gap-3  ">
+                  <p className="text-2xl font-semibold">01</p>
+                  <div>
+                    <BiSolidUpArrow/>
+                    <BiSolidDownArrow/>
+                  </div>
+
+                </div>
               </span>
-              <button>Comprar</button>
-            </div>
+              <button className="bg-azul-escuro text-white w-[222px] md:w-[352px] h-14	 md:h-20 rounded-lg font-semibold text-2xl">Comprar</button>
+            </article>
           </section>
         </body>
       </main>
--- e-commerce/src/pages/Produtos/index.jsx
+++ e-commerce/src/pages/Produtos/index.jsx
@@ -9,8 +9,39 @@ export default function Produtos(){
     return(
         <>
         <Header/>
-        <main className="md:m-20 grid grid-cols-2 md:grid-cols-6 gap-2 md:gap-10 p-4 md:p-10">
-        {arrayDeSapatos.map((sapato)=>(<Destaque sapato={sapato} key={sapato.id}/>))}    
+        <main className="flex md:m-20 md:gap-20  justify-center" >
+            <section className="flex flex-col bg-zinc-50 rounded-lg shadow-lg h-[263px] w-[251px] text-base	text-stone-500 font-semibold">
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" className="form-radio text-blue-500 focus:ring-2 focus:ring-blue-500" name="radioGroup" checked/> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" /> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" /> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" /> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" /> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+                <div className="h-10 flex gap-4 items-center " >
+                    <input type="radio" /> 
+                    <label htmlFor="">Categoria</label>               
+                </div>
+
+
+            </section>
+            <section className=" grid grid-cols-2 md:grid-cols-4 md:gap-10">
+                {arrayDeSapatos.map((sapato)=>(<Destaque sapato={sapato} key={sapato.id}/>))}    
+
+            </section>
             
         </main>
         

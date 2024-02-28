eb1ee32fae5892cc1618f97f1244f7e646abda5a
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-02-28 00:01:20 -0300

    att

--- e-commerce/src/components/CardCategoria/index.jsx
+++ e-commerce/src/components/CardCategoria/index.jsx
@@ -6,7 +6,6 @@ export default function CardCategoria(props){
         <Link to={`/produtos?categoria=${props.categoria}`}>
         
         <main className="flex items-center shadow-xl  bg-slate-100 rounded-lg w-52 h-24       ">
-
             <img src="../src/assets/images/shoe.png" alt="" className="h-20 w-20" />
             <p className="mx-3 font-Inter font-semibold text-base">{props.categoria}</p>
 
